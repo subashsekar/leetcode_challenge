ed3a634ba8ce7014663526faa5a3431766b955de
subashsekar <110082753+subashsekar@users.noreply.github.com>
2026-08-04 17:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (12.51%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        n = len(arr)
+        i = 0
+        while i < n:
+            if arr[i] == 0:
+                arr.insert(i,0)
+                arr.pop()
+                i += 2
+            else:
+                i += 1
+        
