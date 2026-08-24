1473ddce77179b6f5b4de9bc222e877efb357faa
subashsekar <110082753+subashsekar@users.noreply.github.com>
2026-08-24 15:05:29 +0530

    Time: 2 ms (65.19%) | Memory: 21.1 MB (69.83%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,12 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        emp = {}
+        emp = 0
         for i in nums:
-            if i not in emp:
-                emp[i] =1
-            else:
-                emp[i] +=1
-        for num,value in emp.items():
-            if value == 1:
-                return num
+            emp ^= i
+        return emp
         
