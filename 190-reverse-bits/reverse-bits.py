72e4109320062679d59b3df2b6a22ade84b2772f
subashsekar <110082753+subashsekar@users.noreply.github.com>
2026-08-26 17:33:01 +0530

    Time: 72 ms (5.30%) | Memory: 19.2 MB (64.29%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -3,7 +3,11 @@ class Solution:
         result = 0
         for _ in range(32):
             bit = n & 1
+            print("bit",bit)
             result = (result << 1) | bit
+            print("result",result)
+            print("n0",n)
             n >>= 1
+            print("n",n)
         return result
         
